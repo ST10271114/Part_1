162c9222b2a52d9fcf18351e6b19ffd751adf26d
Luwiyane Nhambir <ST10271114@rcconnect.edu.za>
2024-05-13 21:38:02 +0000

    Added the get and set for food group and calories

---
+++ POE_1/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POE_1
+{
+    internal class Ingredients
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public double InitialQuantity { get; set; } 
+        public string Unit { get; set; }
+        public double Calories {  get; set; }
+        public string FoodGroup {  get; set; }
+    }
+
+
+}
